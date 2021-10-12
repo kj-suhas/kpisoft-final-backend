58fff14be42ec3378ae55b0d2e3ee7a087da635f
Suhas K J <suhas.kj@ADKPISOFT.COM>
2021-10-12 11:26:06 +0530

    Initial set up for Spring Boot

---
+++ src/main/java/com/kpisoft/backend/SkylarkHomeBack/SkylarkHomeBackApplication.java
@@ -0,0 +1,13 @@
+package com.kpisoft.backend.SkylarkHomeBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylarkHomeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylarkHomeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpisoft/backend/SkylarkHomeBack/SkylarkHomeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpisoft.backend.SkylarkHomeBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylarkHomeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
